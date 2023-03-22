3a4317ce2689175d4fd203bcb9abc8730e0531f7
ranafge <ranafge@gmail.com>
2023-03-22 17:27:00 +0600

    rust struct and its implementations

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,28 @@
+// struct is called structure custome data type
 
+// impl is called implementation block followed by same name of structure
 
-fn main() {
-  let weather_vec = vec![
-    vec!["Dahaka", "cloudy", "sumy", "22"],
-    vec!["Rangpur", "SUNY", "333"]
-  ];
-
-  for mut my_vec in weather_vec {
-    println!("The city is {}", my_vec[0]);
-
-    while let Some(information) = my_vec.pop()  {
-        if let Ok(number) = information.parse::<i32>()  {
-            println!("The number is {}", number)
-        }
+struct Employee{
+    emp_id: u16,
+    emp_name: String
+}
+impl Employee {
+    fn show_details(&self) {
+        println!("Employee id is {}", self.emp_id);
+        println!("Employee name is {}", self.emp_name);
     }
-  }
-  
+    fn mut_show_employee_name(&mut self, new_name:String){
+        self.emp_name = new_name;
+        println!("Employee new name is {}", self.emp_name );
+    }
+    fn create_employee(id: u16, name: String) -> Employee{
+        Employee { emp_id: id, emp_name: name }
+    }
+}
+fn main() {
+    let em1 = Employee{emp_id:1, emp_name: "amole".to_string()};
+    println!("{:?}", em1.show_details());
+    let employee = Employee::create_employee(22, "ayesha".to_string());
+    employee.show_details();
+
 }
